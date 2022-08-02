986a2e6aaf41f64fc381b41c81e8c627489bc200
Ridho Prasetya <ndiing.inc@gmail.com>
2022-08-02 07:09:35 +0700

    save readme

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# fetch
- nodejs fetch with localStorage and cookieStore
+
+# Fetch
+nodejs fetch resource like `window.fetch`
+
+### Install
+```
+npm install @ndiing/fetch
+```
+
+### Usage
+
+```js
+const fetch = require('@ndiing/fetch')
+
+fetch('http://jsonplaceholder.typicode.com/posts')
+.then(res=>res.json())
+.then(console.log)
+.catch(console.error)
+
+// when request has set-cookie
+// the cookie will be stored in ./data/hostname/default.json
+// and used for next request
+
+// Use Storage
+const pool=Data.pool('jsonplaceholder.typicode.com')
+
+// Get cookie
+pool.cookie
+
+// Set Cookie
+pool.cookie='name=value'
+pool.cookie=[
+    'name=value',
+    'name2=value2',
+]
+
+// Use localStorage
+pool.localStorage
+
+// localStorage methods
+pool.localStorage.getItem('name')
+pool.localStorage.setItem('name','value')
+
+// and another method same with `window.localStorage`
+
+// Use cookieStore
+pool.cookiStore
+
+// all method same with `window.cookieStore`
+```
--- index.js
+++ index.js
@@ -337,8 +337,50 @@ fetch.Request = Request;
 fetch.Response = Response;
 module.exports = fetch;
 
-// Usage
-// fetch("https://mitra.tokopedia.com/")
-// .then((res) => res.text());
+// # Fetch
+// nodejs fetch resource like `window.fetch`
+
+// ### Install
+// ```
+// npm install @ndiing/fetch
+// ```
+
+// ### Usage
+
+// const fetch = require('@ndiing/fetch')
+
+// fetch('http://jsonplaceholder.typicode.com/posts')
+// .then(res=>res.json())
 // .then(console.log)
-// .catch(console.error);
+// .catch(console.error)
+
+// when request has set-cookie
+// the cookie will be stored in ./data/hostname/default.json
+// and used for next request
+
+// Use Storage
+// const pool=Data.pool('jsonplaceholder.typicode.com')
+
+// Get cookie
+// pool.cookie
+
+// Set Cookie
+// pool.cookie='name=value'
+// pool.cookie=[
+//     'name=value',
+//     'name2=value2',
+// ]
+
+// Use localStorage
+// pool.localStorage
+
+// localStorage methods
+// pool.localStorage.getItem('name')
+// pool.localStorage.setItem('name','value')
+
+// and another method same with `window.localStorage`
+
+// Use cookieStore
+// pool.cookiStore
+
+// all method same with `window.cookieStore`
